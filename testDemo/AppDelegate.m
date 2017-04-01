fc271ada002a92360828775a33e5b3eeeae573de
Jason's iMac <1348748408@qq.com>
2017-04-01 09:56:41 +0800

    init

--- testDemo/AppDelegate.m
+++ testDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <JavaScriptCore/JavaScriptCore.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
+
+    
+    
+    
+    
     return YES;
 }
 
--- testDemo/ViewController.m
+++ testDemo/ViewController.m
@@ -17,7 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    for(NSString *familyName in [UIFont familyNames]){
+        NSLog(@"Font FamilyName = %@\n\n",familyName); //*输出字体族科名字
+        
+        for(NSString *fontName in [UIFont fontNamesForFamilyName:familyName]) {
+            NSLog(@"t%@",fontName);         //*输出字体族科下字样名字
+        }
+    }
+
+    
 }
+/**
+ *  点击旋转按钮
+ */
+- (IBAction)rotateAction:(UIButton *)sender {
+    NSArray * array = @[@"1",@"2"];
+    [[UIDevice currentDevice] setValue:[NSNumber numberWithInteger:UIDeviceOrientationLandscapeLeft] forKey:@"orientation"];
+//    [NSException raise:@"出现了一个错误%@" format:@"hello world"];
+//    NSString * str = array[0];
+    
+    
+}
+
+
+////是否支持旋转屏幕
+//- (BOOL)shouldAutorotate{
+//    return YES;
+//}
+////支持哪些方向
+//- (UIInterfaceOrientationMask)supportedInterfaceOrientations{
+//    return UIInterfaceOrientationMaskLandscape;
+//}
+////默认显示的方向
+//- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation{
+//    return UIInterfaceOrientationLandscapeLeft;
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
